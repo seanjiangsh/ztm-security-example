ac52f459304cd0a639484b5b7066605449e73d75
sean <seanjiangsh@gmail.com>
2023-03-29 16:41:49 +0800

    Added passport and strategy for Google OAuth2

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,14 @@ import https, { ServerOptions } from "https";
 import express from "express";
 import helmet from "helmet";
 import path from "path";
+import dotenv from "dotenv";
+import passport, { Profile } from "passport";
+import googleOAuth2, {
+  StrategyOptions,
+  VerifyCallback,
+} from "passport-google-oauth20";
 
+dotenv.config();
 const app = express();
 const PORT = 3000;
 const tlsArgs: ServerOptions = {
@@ -11,8 +18,28 @@ const tlsArgs: ServerOptions = {
   cert: fs.readFileSync("tls/cert.pem"),
 };
 
-app.use(helmet());
+// * setup OAuth2 for passport
+const { clientID, clientSecret } = process.env;
+if (!clientID || !clientSecret)
+  throw new Error("Provide Google OAuth2 info in .env file");
+const authOptions: StrategyOptions = {
+  callbackURL: "/auth/google/callback",
+  clientID,
+  clientSecret,
+};
+function verifyCallback(
+  accessToken: string,
+  refreshToken: string,
+  profile: Profile,
+  done: VerifyCallback
+) {
+  console.log("Google profile", profile);
+  done(null, profile);
+}
+passport.use(new googleOAuth2.Strategy(authOptions, verifyCallback));
 
+app.use(helmet());
+app.use(passport.initialize());
 // function checkLoggedIn(req, res, next) {
 //   const isLoggedIn = true; // TODO
 //   if (!isLoggedIn) return res.status(401).json({ error: "You must log in" });
